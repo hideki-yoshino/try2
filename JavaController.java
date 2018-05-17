799abde2b29c0ecbdfa1a3f425ff39244f64e604
英樹 <yoshinohideki@yoshinohideki-no-iMac.local>
2018-05-17 23:42:44 +0900

    no message

--- JavaController.java
+++ JavaController.java
@@ -20,6 +20,7 @@ import jp.co.sample.java.Parent;
 @RequestMapping(value = "/java")
 public class JavaController {
 
+
 	private static final String TITLE_JABA_SELECTION = "JAVA基礎確認結果画面";
 
 	@InitBinder
